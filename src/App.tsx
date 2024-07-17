ae78643e7677a51425523466e88ca7ad68652db2
José Ricardo <josericardosaraiva7@gmail.com>
2024-07-16 21:18:06 -0300

    feat: create section services

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Briefcase, CalendarBlank, EnvelopeSimple, GithubLogo, LinkedinLogo, TagChevron, WhatsappLogo } from "@phosphor-icons/react"
+import { BracketsAngle, Briefcase, CalendarBlank, Code, DeviceMobileCamera, Devices, EnvelopeSimple, FigmaLogo, Files, GithubLogo, HardDrives, LinkedinLogo, TagChevron, WhatsappLogo } from "@phosphor-icons/react"
 import cv from "./assets/cv.pdf"
 import ImgMe from "./assets/me.png"
 import { toast } from 'react-toastify'
@@ -15,21 +15,23 @@ export function App() {
 
   return (
     <div className="h-screen">
-      <header className="bg-zinc-950 flex justify-center  h-16 w-screen fixed top-0">
+      <header className="bg-zinc-950 flex justify-center  h-18 w-screen fixed top-0">
         <div className="w-[90%] flex items-center p-4 justify-between border-b-2 border-[#151515]">
           <h1 className="text-2xl">{'<JR/>'}</h1>
           <ul className="flex gap-10">
             <li><a href="#about">Sobre</a></li>
             <li><a href="#experience">Experiência</a></li>
+            <li><a href="#experience">Serviços</a></li>
             <li><a href="#projects">Projetos</a></li>
           </ul>
         </div>
       </header>
 
-      <main className="flex justify-center mt-32 h-[90%]">
-        <div className="flex items-center justify-between w-[90%] p-20">
 
-          <div className="text-2xl">
+      <main className=" flex flex-col  items-center">
+
+        <div className="flex items-center justify-between w-[90%] h-[1000px] p-20">
+          <div className="text-2xl mb-20">
             <h1>Olá, me chamo</h1>
             <span className="text-5xl font-bold text-[#00A3FF]">José Ricardo</span>
             <h1>Desenvolvedor Full Stack</h1>
@@ -41,23 +43,21 @@ export function App() {
             </a>
           </div>
 
-          <div id="gradient" className="rounded-full p-24">
-            <img src={ImgMe} alt="developer" className="w-96 rounded-b-full"/>
+          <div className="rounded-full p-10 mb-24">
+            <img src={ImgMe} alt="developer" className="w-[500px] rounded-b-full border-b-8 border-[#00A3FF]"/>
           </div>
-
         </div>
-      </main>
+   
 
 
-      <main id="about" className="flex bg-[#151515] justify-center mt-24 h-[90%]">
-        <div className="w-[70%] p-20">
+        <div id="about" className="w-[100%] bg-[#151515] flex flex-col  items-center p-20">
 
-          <div className="flex w-[100%] flex items-center justify-center gap-2 text-[#00A3FF] mt-10">
-            <TagChevron size={32}  weight="thin" />
-            <h1 className="text-2xl">Sobre mim</h1>
+          <div className="flex w-[100%] items-center justify-center gap-2 text-[#00A3FF] mt-10">
+            <TagChevron size={32}  weight="fill" />
+            <h1 className="text-3xl">Sobre mim</h1>
           </div>
 
-          <p className="mt-10">Sou aluno do curso de tecnologia em Análise e Desenvolvimento de sistemas no Instituto Federal do Piauí. 
+          <p className="mt-10 w-[80%]">Sou aluno do curso de tecnologia em Análise e Desenvolvimento de sistemas no Instituto Federal do Piauí. 
             onde ingressei em 2020, desde então estudo frequentemente vária tecnologias, sendo as de Front End que mais me chamam atenção, 
             no entanto, estudo também o desenvolvimento de Rest APIs e aplicativos multiplataforma. Atualmente focado em ser um desenvolvedor Full Stack
             focado no universo do Javascript.</p>
@@ -101,15 +101,13 @@ export function App() {
           </ul>
 
         </div>
-      </main>
+   
 
 
-      <main id="experience" className="flex justify-center mt-24 h-[90%]">
-        <div className="w-[90%] p-20">
-
+        <div id="experience" className="w-[90%] p-20">
           <div className="w-[100%] flex items-center justify-center gap-2 text-[#00A3FF] mb-10">
-            <Briefcase size={32}  weight="thin" />
-            <h1 className="text-2xl">Experiência</h1>
+            <Briefcase size={32}  weight="fill" />
+            <h1 className="text-3xl">Experiência</h1>
           </div>
 
           <div className="flex items-center  border-b-2 border-[#151515]  justify-center">
@@ -173,9 +171,65 @@ export function App() {
           </div>
 
         </div>
+
+
+        <div id="services" className="w-[90%] p-20">
+
+          <div className="w-[100%] flex items-center justify-center gap-2 text-[#00A3FF] mb-10">
+            <Code size={36}  weight="fill" />
+            <h1 className="text-3xl">Serviços</h1>
+          </div>
+
+          <div className="w-[100%] flex items-center justify-center mt-20">
+            <ul className="flex justify-center gap-10 flex-wrap">
+              <li className="w-[374px] h-[210px] p-6 rounded-md bg-[#151515] shadow-shape border-b-2 border-[#00A3FF]">
+                <div>
+                  <BracketsAngle size={42} color="#00A3FF" />
+                  <h1 className="font-bold text-[22px] mt-20">Criação de sites</h1>
+                </div>
+              </li>
+
+              <li className="w-[374px] h-[210px] p-6 rounded-md bg-[#151515] shadow-shape border-b-2 border-[#00A3FF]">
+                <div>
+                  <DeviceMobileCamera size={42} color="#00A3FF" />
+                  <h1 className="font-bold text-[22px] mt-20">Criação de aplicativos</h1>
+                </div>
+              </li>
+
+              <li className="w-[374px] h-[210px] p-6 rounded-md bg-[#151515] shadow-shape border-b-2 border-[#00A3FF]">
+                <div>
+                  <Devices  size={42} color="#00A3FF" />
+                  <h1 className="font-bold text-[22px] mt-20">Sites responsivos</h1>
+                </div>
+              </li>
+
+              <li className="w-[374px] h-[210px] p-6 rounded-md bg-[#151515] shadow-shape border-b-2 border-[#00A3FF]">
+                <div>
+                  <HardDrives size={42} color="#00A3FF" />
+                  <h1 className="font-bold text-[22px] mt-20">Criação de APIs</h1>
+                </div>
+              </li>
+
+              <li className="w-[374px] h-[210px] p-6 rounded-md bg-[#151515] shadow-shape border-b-2 border-[#00A3FF]">
+                <div>
+                  <Files size={42} color="#00A3FF" />
+                  <h1 className="font-bold text-[22px] mt-16">Consultoria de desenvolvimento de sistemas</h1>
+                </div>
+              </li>
+
+              <li className="w-[374px] h-[210px] p-6 rounded-md bg-[#151515] shadow-shape border-b-2 border-[#00A3FF]">
+                <div>
+                  <FigmaLogo  size={42} color="#00A3FF" />
+                  <h1 className="font-bold text-[22px] mt-20">Front end de alta fidelidade</h1>
+                </div>
+              </li>
+
+            </ul>
+          </div>
+
+        </div>
+
       </main>
-
-
     </div>
   )
 }
